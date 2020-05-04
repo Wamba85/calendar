30d7adc4f2ae4d4b26a1b536ba03ab18006ff292
Andrea Mori <wamba85@gmail.com>
2020-05-04 21:57:26 +0200

    add weekdays

--- src/Calendar/Calendar.tsx
+++ src/Calendar/Calendar.tsx
@@ -3,13 +3,13 @@ import React from 'react'
 const Calendar = () => {
   return (
     <div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+      <div className='weekday'>Sun</div>
+      <div className='weekday'>Mon</div>
+      <div className='weekday'>Tue</div>
+      <div className='weekday'>Wed</div>
+      <div className='weekday'>Thu</div>
+      <div className='weekday'>Fri</div>
+      <div className='weekday'>Sat</div>
     </div>
   )
 }
