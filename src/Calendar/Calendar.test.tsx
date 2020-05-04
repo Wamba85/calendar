a49370b3736f93b4c43da779e84300b627e1a5dc
Andrea Mori <wamba85@gmail.com>
2020-05-04 21:57:06 +0200

    add weekday test

--- src/Calendar/Calendar.test.tsx
+++ src/Calendar/Calendar.test.tsx
@@ -14,4 +14,9 @@ describe('Calendar', () => {
   it('should render 8 divs', () => {
     expect(wrapper.find('div').length).toEqual(8)
   })
+
+  it('should have week days text', () => {
+    expect(wrapper.find('.weekday').first().text()).toBe('Sun')
+    expect(wrapper.find('.weekday').last().text()).toBe('Sat')
+  })
 })
