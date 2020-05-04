c377e53c7cc59dd09f7885963cae13b23ed62d57
Andrea Mori <wamba85@gmail.com>
2020-05-04 22:15:28 +0200

    add timetables

--- src/Calendar/Calendar.tsx
+++ src/Calendar/Calendar.tsx
@@ -10,6 +10,30 @@ const Calendar = () => {
       <div className='weekday'>Thu</div>
       <div className='weekday'>Fri</div>
       <div className='weekday'>Sat</div>
+      <div className='timetables'>00:00</div>
+      <div className='timetables'>01:00</div>
+      <div className='timetables'>02:00</div>
+      <div className='timetables'>03:00</div>
+      <div className='timetables'>04:00</div>
+      <div className='timetables'>05:00</div>
+      <div className='timetables'>06:00</div>
+      <div className='timetables'>07:00</div>
+      <div className='timetables'>08:00</div>
+      <div className='timetables'>09:00</div>
+      <div className='timetables'>10:00</div>
+      <div className='timetables'>11:00</div>
+      <div className='timetables'>12:00</div>
+      <div className='timetables'>13:00</div>
+      <div className='timetables'>14:00</div>
+      <div className='timetables'>15:00</div>
+      <div className='timetables'>16:00</div>
+      <div className='timetables'>17:00</div>
+      <div className='timetables'>18:00</div>
+      <div className='timetables'>19:00</div>
+      <div className='timetables'>20:00</div>
+      <div className='timetables'>21:00</div>
+      <div className='timetables'>22:00</div>
+      <div className='timetables'>23:00</div>
     </div>
   )
 }
