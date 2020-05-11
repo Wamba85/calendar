b74f1892b89546ddf80f4ef5775a8ce62f73df60
Andrea Mori <wamba85@gmail.com>
2020-05-11 15:07:37 +0200

    add programatically render and module classes

--- src/Calendar/Calendar.tsx
+++ src/Calendar/Calendar.tsx
@@ -1,42 +1,59 @@
 import React from 'react'
+import styles from './Calendar.module.css'
 
 const Calendar = (): JSX.Element => {
+  const eventCols = []
+  for (let i = 0; i < 7; i++) {
+    let eventSlots = []
+    for (let j = 0; j < 24; j++) {
+      eventSlots.push(<div key={`event${i}-${j}`} className={styles.eventSlot}>{`${i}-${j}`}</div>)
+    }
+    eventCols.push(
+      <div key={`eventCol${i}`} className={styles.eventCol}>
+        {eventSlots}
+      </div>,
+    )
+  }
   return (
     <div>
-      <div>
-        <div className="weekday">Sun</div>
-        <div className="weekday">Mon</div>
-        <div className="weekday">Tue</div>
-        <div className="weekday">Wed</div>
-        <div className="weekday">Thu</div>
-        <div className="weekday">Fri</div>
-        <div className="weekday">Sat</div>
+      <div className={styles.weekdayContainer}>
+        <div className={styles.weekday}></div>
+        <div className={styles.weekday}>Sun</div>
+        <div className={styles.weekday}>Mon</div>
+        <div className={styles.weekday}>Tue</div>
+        <div className={styles.weekday}>Wed</div>
+        <div className={styles.weekday}>Thu</div>
+        <div className={styles.weekday}>Fri</div>
+        <div className={styles.weekday}>Sat</div>
       </div>
-      <div>
-        <div className="timetables">00:00</div>
-        <div className="timetables">01:00</div>
-        <div className="timetables">02:00</div>
-        <div className="timetables">03:00</div>
-        <div className="timetables">04:00</div>
-        <div className="timetables">05:00</div>
-        <div className="timetables">06:00</div>
-        <div className="timetables">07:00</div>
-        <div className="timetables">08:00</div>
-        <div className="timetables">09:00</div>
-        <div className="timetables">10:00</div>
-        <div className="timetables">11:00</div>
-        <div className="timetables">12:00</div>
-        <div className="timetables">13:00</div>
-        <div className="timetables">14:00</div>
-        <div className="timetables">15:00</div>
-        <div className="timetables">16:00</div>
-        <div className="timetables">17:00</div>
-        <div className="timetables">18:00</div>
-        <div className="timetables">19:00</div>
-        <div className="timetables">20:00</div>
-        <div className="timetables">21:00</div>
-        <div className="timetables">22:00</div>
-        <div className="timetables">23:00</div>
+      <div className={styles.eventColsContainer}>
+        <div className={styles.timetablesContainer}>
+          <div className={styles.timetable}>00:00</div>
+          <div className={styles.timetable}>01:00</div>
+          <div className={styles.timetable}>02:00</div>
+          <div className={styles.timetable}>03:00</div>
+          <div className={styles.timetable}>04:00</div>
+          <div className={styles.timetable}>05:00</div>
+          <div className={styles.timetable}>06:00</div>
+          <div className={styles.timetable}>07:00</div>
+          <div className={styles.timetable}>08:00</div>
+          <div className={styles.timetable}>09:00</div>
+          <div className={styles.timetable}>10:00</div>
+          <div className={styles.timetable}>11:00</div>
+          <div className={styles.timetable}>12:00</div>
+          <div className={styles.timetable}>13:00</div>
+          <div className={styles.timetable}>14:00</div>
+          <div className={styles.timetable}>15:00</div>
+          <div className={styles.timetable}>16:00</div>
+          <div className={styles.timetable}>17:00</div>
+          <div className={styles.timetable}>18:00</div>
+          <div className={styles.timetable}>19:00</div>
+          <div className={styles.timetable}>20:00</div>
+          <div className={styles.timetable}>21:00</div>
+          <div className={styles.timetable}>22:00</div>
+          <div className={styles.timetable}>23:00</div>
+        </div>
+        {eventCols}
       </div>
     </div>
   )
