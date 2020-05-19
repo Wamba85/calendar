d3a89deb52d1f2c9861c94e2fd57964ca03ed33f
Andrea Mori <wamba85@gmail.com>
2020-05-19 15:27:29 +0200

    add inputs

--- src/Modal/Modal.tsx
+++ src/Modal/Modal.tsx
@@ -23,14 +23,20 @@ const Modal = ({ open, handleClose, oraDa, oraA }: Props): JSX.Element => {
       <DialogTitle id="form-dialog-title">Nuova Visita</DialogTitle>
       <DialogContent>
         <DialogContentText>{`ora da: ${oraDa} ora a: ${oraA}`}</DialogContentText>
-        <TextField autoFocus margin="dense" id="name" label="Email Address" type="email" fullWidth />
+        <TextField autoFocus margin="dense" label="Nominativo" type="text" fullWidth />
+        <TextField margin="dense" label="Data di nascita" type="text" fullWidth />
+        <TextField margin="dense" label="Codice Fiscale" type="text" fullWidth />
+        <TextField margin="dense" label="Cellulare" type="email" fullWidth />
+        <TextField margin="dense" label="Telefono" type="email" fullWidth />
+        <TextField margin="dense" label="Email" type="email" fullWidth />
+        <TextField margin="dense" label="Descrizione visita" type="text" multiline rows={10} fullWidth />
       </DialogContent>
       <DialogActions>
         <Button onClick={handleClose} color="primary">
-          Cancel
+          Annulla
         </Button>
         <Button onClick={handleClose} color="primary">
-          Subscribe
+          Inserisci
         </Button>
       </DialogActions>
     </Dialog>
