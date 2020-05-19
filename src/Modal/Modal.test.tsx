3356b6229c2442450079a1136fab7a22541ac5ed
Andrea Mori <wamba85@gmail.com>
2020-05-19 13:46:41 +0200

    add title time test

--- src/Modal/Modal.test.tsx
+++ src/Modal/Modal.test.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { shallow, ShallowWrapper } from 'enzyme'
 import Modal from './Modal'
-import { Dialog } from '@material-ui/core'
+import { Dialog, DialogTitle, DialogContentText } from '@material-ui/core'
 
 describe('Calendar', () => {
   let wrapper: ShallowWrapper
   const mock = jest.fn()
 
   beforeEach(() => {
-    wrapper = shallow(<Modal open={false} handleClose={mock} />)
+    wrapper = shallow(<Modal open={false} handleClose={mock} oraDa={''} oraA={''} />)
   })
 
   afterEach(() => {
@@ -17,5 +17,14 @@ describe('Calendar', () => {
 
   it('should render a Dialog', () => {
     expect(wrapper.find(Dialog).length).toEqual(1)
+  })
+
+  it('should have a title', () => {
+    expect(wrapper.find(DialogTitle).text()).toBe('Nuova Visita')
+  })
+
+  it('should display the correct time range', () => {
+    wrapper.setProps({ oraDa: '10:00', oraA: '10:10' })
+    expect(wrapper.find(DialogContentText).text()).toBe('ora da: 10:00 ora a: 10:10')
   })
 })
