93c57a33edca29b31185f69ee9bb3c8bbe4bc747
Andrea Mori <wamba85@gmail.com>
2020-05-19 13:47:39 +0200

    add calendar theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import './App.css'
 import Calendar from './Calendar/Calendar'
+import { ThemeProvider } from '@material-ui/core'
+import theme from './styles/calendarTheme'
 
 function App(): JSX.Element {
   return (
     <div className="App">
-      <Calendar timeslot={30} weekdays={[12, 13, 14, 15, 16, 17, 18]} />
+      <ThemeProvider theme={theme}>
+        <Calendar timeslot={30} weekdays={[12, 13, 14, 15, 16, 17, 18]} />
+      </ThemeProvider>
     </div>
   )
 }
