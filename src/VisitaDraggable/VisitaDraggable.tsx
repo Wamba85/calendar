5ad1114b6fe398bba59b8b915d9fecffab2cd74c
Andrea Mori <wamba85@gmail.com>
2020-05-26 22:30:19 +0200

    add confirm/cancel behaviour

--- src/VisitaDraggable/VisitaDraggable.tsx
+++ src/VisitaDraggable/VisitaDraggable.tsx
@@ -1,5 +1,6 @@
-import React, { useRef } from 'react'
-import Draggable, { DraggableEventHandler } from 'react-draggable'
+import React, { useRef, useState } from 'react'
+import Draggable, { DraggableEventHandler, DraggableEvent, DraggableData } from 'react-draggable'
+import { Dialog, DialogTitle, DialogActions, Button } from '@material-ui/core'
 import styles from './VisitaDraggable.module.css'
 
 type Props = {
@@ -11,46 +12,79 @@ type Props = {
 }
 
 const VisitaDraggable = ({ width, id, day, time, handleEndDrag }: Props): JSX.Element => {
+  const [open, setOpen] = useState(false)
+  const [x, setX] = useState(0)
+  const [y, setY] = useState(0)
+  const [tempDay, setTempDay] = useState(0)
+  const [tempTime, setTempTime] = useState(0)
+
   const nodeRef = useRef(null)
-  let tempDay = 0,
-    tempTime = 0
+
   const handleDrag: DraggableEventHandler = (e, ui) => {
     console.log(ui.deltaX)
     console.log(ui.deltaY)
+
     if (ui.deltaX > 0) {
-      tempDay++
+      setTempDay(tempDay + 1)
     }
     if (ui.deltaX < 0) {
-      tempDay--
+      setTempDay(tempDay - 1)
     }
     if (ui.deltaY > 0) {
-      tempTime++
+      setTempTime(tempTime + 1)
     }
     if (ui.deltaY < 0) {
-      tempTime--
+      setTempTime(tempTime - 1)
     }
   }
 
-  const handleDragStop: DraggableEventHandler = (e) => {
-    e.stopPropagation()
-    if (window.confirm('Vuoi davvero spostare la visita?')) {
-      handleEndDrag(id, tempDay, tempTime)
-    }
+  const handleDragStop: DraggableEventHandler = (e: DraggableEvent, data: DraggableData) => {
+    console.log(data)
+    setX(data.x - data.deltaX)
+    setY(data.y - data.deltaY)
+    setOpen(true)
+  }
+
+  const handleCloseCancel = (): void => {
+    console.log('close')
+    setX(0)
+    setY(0)
+    setTempTime(0)
+    setTempDay(0)
+    setOpen(false)
+  }
+
+  const handleCloseConfirm = (): void => {
+    setOpen(false)
+    handleEndDrag(id, tempDay, tempTime)
   }
 
   return (
-    <Draggable
-      // eslint-disable-next-line
-      // @ts-ignore
-      nodeRef={nodeRef}
-      grid={[width, 50]}
-      bounds={'#eventSlotContainer'}
-      onDrag={handleDrag}
-      onStop={handleDragStop}
-      position={{ x: 0, y: 0 }}
-    >
-      <div ref={nodeRef} className={styles.draggableDiv}>{`${day} ${time}`}</div>
-    </Draggable>
+    <React.Fragment>
+      <Draggable
+        // eslint-disable-next-line
+        // @ts-ignore
+        nodeRef={nodeRef}
+        grid={[width, 50]}
+        bounds={'#eventSlotContainer'}
+        onDrag={handleDrag}
+        onStop={handleDragStop}
+        position={{ x: x, y: y }}
+      >
+        <div ref={nodeRef} className={styles.draggableDiv}>{`${day} ${time}`}</div>
+      </Draggable>
+      <Dialog open={open} onClose={handleCloseCancel} aria-labelledby="form-dialog-title">
+        <DialogTitle>Vuoi spostare la visita?</DialogTitle>
+        <DialogActions>
+          <Button onClick={handleCloseCancel} color="primary">
+            Annulla
+          </Button>
+          <Button onClick={handleCloseConfirm} color="primary">
+            Inserisci
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </React.Fragment>
   )
 }
 
